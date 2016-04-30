f89f4b6a3f3de95943f9cb78ccb02cef3da9e156
Joey Fang <linzhoufang@gmail.com>
2016-04-30 12:40:00 -0700

    Configurable elevator no.
    2016-04-30
    
    0.0 : began to list simple vars.

---
+++ Devil/Devil/I-O.cpp
@@ -0,0 +1,337 @@
+/* WORKLOG
+ * 2015-12-31
+ * Starting I/O coding.
+ *
+ */
+
+/* I-O.cpp
+ *
+ *
+ */
+
+/* === LIBRARIES === */
+
+
+#include "I-O.h"
+
+/* I-O.h
+ * Headers for I/O fxns.
+ */
+
+
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ *********************************
+ *** ACTIVECLASS - DISPATCHER ****
+ *********************************
+ */
+
+class Dispatcher : public ActiveClass
+{
+
+private:
+	int rendezvousCount;
+	int numElevator;
+
+public:
+	Dispatcher(int _numElevator, int _rendezvousCount)
+	{
+		numElevator = _numElevator;
+		rendezvousCount = _rendezvousCount;
+	}
+
+	~Dispatcher(){}
+
+private:
+	int main(void)
+	{
+	
+	/*******************
+	  *** RENDEZVOUS ***
+	  ******************
+	*/
+	cout << asdf << asdf << endl;
+	CRendezvous	DISP_r1	("Rendezvous_InitiateActiveClasses", rendezvousCount);
+	CRendezvous DISP_r2	("Rendezvous_TerminateClasses", rendezvousCount);
+
+	/*******************
+	  *** PIPELINES ****
+	  ******************
+	*/
+	
+	CPipe	DISP_pipe1	("Pipe_IOToDispatcher", 1024) ;
+	CPipe	DISP_pipe2	("Pipe_DispatcherToIO", 1024) ;
+	
+	/*******************
+	  *** MUTEX ********
+	  ******************
+	*/
+
+
+	CMutex* DISP_mutex = new CMutex ("Mutex_General"); // mutex to give writing access.
+	
+	/******************
+	  *** DATAPOOLS ***
+	  *****************
+	*/
+
+	string DISP_DPStringBegin = "Datapool_";
+	string* DISP_DPStringArray_Full = new string[numElevator];
+	for (int i = 0; i < numElevator ; i++) {
+		DISP_DPStringArray_Full[i] = DISP_DPStringBegin + to_string(static_cast<long long>(i));
+	}
+
+	CDataPool** DISP_ElevatorStructArray_DataPool = new CDataPool*[numElevator];
+	elevatorStatus** DISP_ElevatorStructArray_Local = new elevatorStatus*[numElevator];
+	for (int i = 0; i < numElevator ; i++) {
+		DISP_ElevatorStructArray_DataPool[i] = new CDataPool(DISP_DPStringArray_Full[i], sizeof(struct elevatorStatus));
+		DISP_ElevatorStructArray_Local[i] = (elevatorStatus*)(DISP_ElevatorStructArray_DataPool[i]->LinkDataPool()); 
+	}
+
+
+
+	/***********************
+	  ** DISPATCHER LOGIC **
+	  **********************
+	*/
+
+	printf("%d rendC Dispatcher\n", rendezvousCount);
+	DISP_r1.Wait();
+
+	/***********************
+	  ** TERMINATIONS ******
+	  **********************
+	*/
+	delete[] DISP_DPStringArray_Full; // does this require multiple deletes?
+	delete[] DISP_ElevatorStructArray_Local;
+	delete[] DISP_ElevatorStructArray_DataPool;
+	
+	DISP_r2.Wait();
+	return 0;
+	}
+} ;
+
+
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ *******************************
+ *** ACTIVECLASS - ELEVATOR ****
+ *******************************
+*/
+
+class Elevator : public ActiveClass
+{
+private:
+	int myNumber;
+	int rendezvousCount;
+	elevatorStatus myStatus;
+public:
+	Elevator(){}
+	Elevator(int _myNumber, int _rendezvousCount) 
+	{
+		myNumber = _myNumber;
+		rendezvousCount = _rendezvousCount;
+	}
+	~Elevator(){}
+
+private:
+	int Fxn1(void *threaddata1)
+	{
+		//Testing.
+		for (int i = 0; i < 3; i++) {
+			printf("this is Fxn1  -> %d <- \n", myNumber) ;
+			Sleep(5);
+		}
+		return 0;
+	}
+
+	int Fxn2(void *threaddata1)
+	{
+		//Testing.
+		for (int i = 0; i < 3; i++) {
+			printf("this is Fxn2  -> %d <- -> %d <-\n", rand()%100 + 100, myNumber ) ;
+			Sleep(5);
+		}
+		return 0;
+	}
+
+	int main(void)
+	{
+		
+		/*******************
+		  *** RENDEZVOUS ***
+		  ******************
+        */
+		CRendezvous	ELE_r1	("Rendezvous_InitiateActiveClasses", rendezvousCount); //rendezvous point for all programs to start together.
+		CRendezvous ELE_r2	("Rendezvous_TerminateClasses", rendezvousCount);
+		
+		/*******************
+		  *** MUTEX ********
+		  ******************
+		*/
+
+		CMutex* ELE_mutex = new CMutex ("Mutex_General");
+		
+		/******************
+		  *** DATAPOOLS ***
+		  *****************
+		*/
+
+		string ELE_DPString = "Datapool_" + to_string(static_cast<long long>(myNumber));
+
+		CDataPool ELE_ElevatorStruct_DataPool(ELE_DPString, sizeof(struct elevatorStatus));
+		elevatorStatus* ELE_ElevatorStruct_Local = (struct elevatorStatus*)(ELE_ElevatorStruct_DataPool.LinkDataPool());
+
+
+		printf("%d rendEle %d \n", rendezvousCount,myNumber);
+		ELE_r1.Wait();
+		ClassThread<Elevator> Thread1 (this, &Elevator::Fxn1, ACTIVE, NULL) ;
+		ClassThread<Elevator> Thread2 (this, &Elevator::Fxn2, ACTIVE, NULL) ;
+		ClassThread<Elevator> Thread3 (this, &Elevator::Fxn1, ACTIVE, NULL) ;
+		Thread1.WaitForThread();
+		Thread2.WaitForThread();
+		Thread3.WaitForThread();
+
+		ELE_r2.Wait();
+		return 0;
+	}
+} ;
+
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ *******************
+ *** MAIN - I/O ****
+ *******************
+*/
+
+int main() {
+	
+
+	int numElevator = 0;
+	cout << "Input the number of elevators you want: " << endl;
+	cin >> numElevator;
+	cout << "user entered: " << numElevator << endl;;
+	Elevator ** ele;
+	ele = new Elevator*[numElevator];
+	int rendezvousCount = numElevator + 2;
+
+	/*******************
+	  *** RENDEZVOUS ***
+	  ******************
+	*/
+
+	CRendezvous	IO_r1	("Rendezvous_InitiateActiveClasses", rendezvousCount); 
+	CRendezvous IO_r2	("Rendezvous_TerminateClasses", rendezvousCount);
+
+	//rendezvous point for all programs to start together.
+
+	
+	/*******************
+	  *** PIPELINES ****
+	  ******************
+	*/
+	
+	CPipe	pipe1	("PipeIOToDispatcher", 1024) ;
+	CPipe	pipe2	("PipeDispatcherToIO", 1024) ;
+
+	/*******************
+	  *** MUTEX ********
+	  ******************
+	*/
+	
+
+	CMutex* IO_mutex = new CMutex ("Mutex_General");
+
+	/******************
+	  *** DATAPOOLS ***
+	  *****************
+	*/
+
+	string IO_DPStringBegin = "Datapool_";
+	string* IO_DPStringArray_Full = new string[numElevator];
+	for (int i = 0; i < numElevator ; i++) {
+		IO_DPStringArray_Full[i] = IO_DPStringBegin + to_string(static_cast<long long>(i));
+	}
+
+	CDataPool** IO_ElevatorStructArray_DataPool = new CDataPool*[numElevator];
+	struct elevatorStatus** IO_ElevatorStructArray_Local = new elevatorStatus*[numElevator];
+	for (int i = 0; i < numElevator ; i++) {
+		IO_ElevatorStructArray_DataPool[i] = new CDataPool(IO_DPStringArray_Full[i], sizeof(struct elevatorStatus));
+		IO_ElevatorStructArray_Local[i] = (elevatorStatus*)(IO_ElevatorStructArray_DataPool[i]->LinkDataPool()); 
+	}
+
+	
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! gotta do smae thing for pipes
+
+	/*************************
+	  * CLASS INSTANTIATIONS *
+	  ************************
+	*/
+
+	for (int i = 0; i < numElevator; i++) {
+		ele[i] = new Elevator(i,rendezvousCount);
+		cout << /**ele[i] <<*/ "\t" << ele[i] << "\t" << *ele << "\t" << ele << endl;
+	}
+
+	for (int i = 0; i < numElevator; i++) {
+		ele[i]->Resume();
+	}
+
+	Dispatcher disp(numElevator,rendezvousCount);
+	disp.Resume();
+
+	Sleep(10);
+	
+	IO_r1.Wait();
+	
+	cout << "let's go!" << endl;
+	
+
+	/***************
+	  ** IO LOGIC **
+	  **************
+	*/
+
+
+	
+	/*******************
+	 * CLOSING THREADS *
+	 *******************
+	*/
+	
+	delete[] IO_DPStringArray_Full;
+	delete[] IO_ElevatorStructArray_Local;
+	delete[] IO_ElevatorStructArray_DataPool;
+
+	IO_r2.Wait();
+
+	for (int k = 0; k < numElevator; k++) {
+		ele[k]->WaitForThread();
+	}
+	disp.WaitForThread();
+	delete ele;
+
+	cout << "Terminated correctly!" << endl;
+
+
+	//double ** var = new double*[numElevator];
+	//for (int i = 0; i < numElevator; i++) {
+	//	
+	//	var[i]= new double; //0x025f5e40
+	//	*var[i]= i + 100;
+	//	cout << *var[i] << "\t" << var[i] << "\t" << *var << "\t" << var << endl;
+	//	//var[1]= new int; //
+	//	//var[2]= new int; //0x00e35df0
+	//}
+	//delete var;
+
+	//int * ptr = new int[numElevator];
+	//for (int m = 100; m < numElevator + 100; m++) {
+	//	*ptr = m;
+	//	ptr++;
+	//}
+	//for (int n = 0; n < numElevator; n++) {
+	//	printf("\n%d \n",*(--ptr));
+	//}
+
+	//int *ptr2 = new int[10];
+
+	return 0;
+}
---
+++ Devil/Devil/I-O.h
@@ -0,0 +1,72 @@
+#ifndef __IO__
+#define __IO__
+
+#include <stdio.h>
+
+/* stdio.h
+ * printf
+ */
+
+#include <stdlib.h>
+
+/* stdlib.h
+ * 
+ */
+
+//#include <iostream>
+//
+///* iostream // QUES: Why is it not a header file?
+// *
+// */
+//
+//#include <string>
+//
+///* string
+// * - concatenate strings to create DPs and pipelines
+// */
+
+#include "..\..\..\..\RTexample\rt.h"
+
+/* rt.h
+ * Dialog with win32 for multithreaded.
+ */
+
+
+using namespace std;
+
+#endif
+
+/**
+  * Elevator definitions. Class imported from Demon: elevator.h
+  *
+  *
+  */
+#ifndef __ELEVATOR__
+#define __ELEVATOR__
+
+struct 	elevatorStatus 	{	// start of structure template
+		int floor ;			// floor corresponding to lifts current position
+		int usage;
+		int door;			
+		int direction ;		// direction of travel of lift
+		int floors[10] ;	// an array representing the floors and whether requests are set 
+		int lights[10] ;
+		int updir[10];
+		int downdir[10];
+} ;							// end of structure template
+
+static const int STOPPED =	0	;
+static const int UP	=		1	;
+static const int DOWN =		2	;
+
+static const int NOTUSED =	0	;
+static const int USING =	1	;
+
+static const int CLOSED	=	0	;
+static const int OPEN =		1	;
+
+static const int NO_DESTINATION =	11 ;
+
+
+#define IS_CAN_BS1(BS1) ((BS1) <= (uint8_t)0x0F)
+#endif
