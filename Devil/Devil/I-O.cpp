5ea1ce4e5120a0ae25fae8534fd7c144fd7aa45e
Joey Fang <linzhoufang@gmail.com>
2016-05-03 17:08:38 -0700

    Revert "CEN Rev 0.3"
    This reverts commit ebaae96da30f385c58c63c269a9375d13689fc95.

--- Devil/Devil/I-O.cpp
+++ Devil/Devil/I-O.cpp
@@ -15,47 +15,225 @@
 
 
 #include "I-O.h"
+
 // I-O.h :
 // Headers for I/O fxns.
-
-#include "Dispatcher_ActiveClass.h"
-#include "Elevator_ActiveClass.h"
-
-
  
 
 
+/********************************
+*** ACTIVECLASS - DISPATCHER ****
+********************************/
+
+class Dispatcher : public ActiveClass
+{
+
+private:
+	int rendezvousCount;
+	int numElevator;
+
+public:
+	Dispatcher(int _numElevator, int _rendezvousCount)	{
+		numElevator = _numElevator;
+		rendezvousCount = _rendezvousCount;
+	}
+
+	~Dispatcher(){}
+
+private:
+	int main(void)
+	{
+	
+	/*-------------------------------------------------------------------------------------------------------------*/
+	/* Initializing Rendezvous */
+	/*--------------------------------------------------------------------------------------------------------------*/	
+	cout << "asdf" << "asdf" << endl;
+	CRendezvous	DISP_r1	("Rendezvous_InitiateActiveClasses", rendezvousCount);
+	CRendezvous DISP_r2	("Rendezvous_TerminateClasses", rendezvousCount);
+
+	/*--------------------------------------------------------------------------------------------------------------*/
+	/* Initializing Pipelines to IO */
+	/*--------------------------------------------------------------------------------------------------------------*/	
+	CPipe	DISP_pipe1	("Pipe_IOToDispatcher", 1024) ;
+	CPipe	DISP_pipe2	("Pipe_DispatcherToIO", 1024) ;
+	
+	/*--------------------------------------------------------------------------------------------------------------*/
+	/* Initializing Mutex */
+	/*--------------------------------------------------------------------------------------------------------------*/	
+	CMutex* DISP_mutex = new CMutex ("Mutex_General"); // mutex to give writing access.
+	
+	/*--------------------------------------------------------------------------------------------------------------*/
+	/* Initializing Datapools */
+	/* - Concatonates string Datapool_ with numElevator. EG: if numElevator is 3 from user input, then Datapool_0,	*/
+	/* - Datapool_1, Datapool_2 will be created.                                                                    */
+	/*--------------------------------------------------------------------------------------------------------------*/	
+	string DISP_DPStringBegin = "Datapool_";
+	string* DISP_DPStringArray_Full = new string[numElevator];
+	for (int i = 0; i < numElevator ; i++) {
+		DISP_DPStringArray_Full[i] = DISP_DPStringBegin + to_string(static_cast<long long>(i));
+	}
+
+	CDataPool** DISP_ElevatorStructArray_DataPool = new CDataPool*[numElevator]; // !!! know why you used double pointer? or even a pointer?
+	elevatorStatus** DISP_ElevatorStructArray_Local = new elevatorStatus*[numElevator]; //!!! what is this used for?
+	for (int i = 0; i < numElevator ; i++) {
+		DISP_ElevatorStructArray_DataPool[i] = new CDataPool(DISP_DPStringArray_Full[i], sizeof(struct elevatorStatus));
+		DISP_ElevatorStructArray_Local[i] = (elevatorStatus*)(DISP_ElevatorStructArray_DataPool[i]->LinkDataPool()); 
+	}
+
+	/*--------------------------------------------------------------------------------------------------------------*/
+	/* Initializing Pipelines to Elevators */
+	/* - Same concatonation logic as for Datapools, but there are two pipes per elevator, one sends data from       */
+	/* - dispatcher to elevator, and the other receives.                                                            */
+	/*                                                                                                              */
+	/*--------------------------------------------------------------------------------------------------------------*/	
+
+	string DISP_PipeStringBegin_DispToEle = "Pipe_DispatcherToEle_"; // !!!is it a bad idea to start initializations after performing loops?
+	string DISP_PipeStringBegin_EleToDisp = "Pipe_Ele_";
+	string DISP_PipeStringEnd_EleToDisp = "_ToDispatcher";
+	string* DISP_PipeStringArray_DispToEle_Full = new string[numElevator];
+	string* DISP_PipeStringArray_EleToDisp_Full = new string[numElevator];
+
+	for (int i = 0; i < numElevator ; i++) {
+		DISP_PipeStringArray_DispToEle_Full[i] = DISP_PipeStringBegin_DispToEle + to_string(static_cast<long long>(i));
+		DISP_PipeStringArray_EleToDisp_Full[i] = DISP_PipeStringBegin_EleToDisp + to_string(static_cast<long long>(i)) + DISP_PipeStringEnd_EleToDisp;
+	}
+
+	CPipe** DISP_PipeToEle = new CPipe*[numElevator]; // !!! if only pointer, not double pointer, it says no constructor exists for CPipe. why?
+	CPipe** DISP_PipeFromEle = new CPipe*[numElevator];
+
+	for (int i = 0; i < numElevator ; i++) {
+		DISP_PipeToEle[i] = new CPipe(DISP_PipeStringArray_DispToEle_Full[i]); //!!! why is new CPipe here fine? and not above?
+		DISP_PipeFromEle[i] = new CPipe(DISP_PipeStringArray_EleToDisp_Full[i]); 
+	}
+
+	/*--------------------------------------------------------------------------------------------------------------*/
+	/* Dispatcher Logic */
+	/*--------------------------------------------------------------------------------------------------------------*/	
+
+	printf("%d rendC Dispatcher\n", rendezvousCount);
+	DISP_r1.Wait();
+
+	/*--------------------------------------------------------------------------------------------------------------*/
+	/* Terminate Arrays */
+	/*--------------------------------------------------------------------------------------------------------------*/	
+	delete[] DISP_DPStringArray_Full; // does this require multiple deletes?
+	delete[] DISP_ElevatorStructArray_Local;
+	delete[] DISP_ElevatorStructArray_DataPool;
+	
+	DISP_r2.Wait();
+	return 0;
+	}
+} ;
+
+
+/******************************
+*** ACTIVECLASS - ELEVATOR ****
+******************************/
+
+class Elevator : public ActiveClass
+{
+private:
+	int myNumber;
+	int rendezvousCount;
+	elevatorStatus myStatus;
+public:
+	Elevator(){}
+	Elevator(int _myNumber, int _rendezvousCount) {
+		myNumber = _myNumber;
+		rendezvousCount = _rendezvousCount;
+	}
+	~Elevator(){}
+
+private:
+	int Fxn1(void *threaddata1) {
+		//Testing.
+		for (int i = 0; i < 3; i++) {
+			printf("this is Fxn1  -> %d <- \n", myNumber) ;
+			Sleep(5);
+		}
+		return 0;
+	}
+
+	int Fxn2(void *threaddata1) {
+		//Testing.
+		for (int i = 0; i < 3; i++) {
+			printf("this is Fxn2  -> %d <- -> %d <-\n", rand()%100 + 100, myNumber ) ;
+			Sleep(5);
+		}
+		return 0;
+	}
+
+	int main(void)
+	{
+		
+		/*-------------------------------------------------------------------------------------------------------------*/
+		/* Initializing Rendezvous */
+		/*-------------------------------------------------------------------------------------------------------------*/	
+		CRendezvous	ELE_r1	("Rendezvous_InitiateActiveClasses", rendezvousCount); //rendezvous point for all programs to start together.
+		CRendezvous ELE_r2	("Rendezvous_TerminateClasses", rendezvousCount);
+		
+		/*-------------------------------------------------------------------------------------------------------------*/
+		/* Initializing Mutexes */
+		/*-------------------------------------------------------------------------------------------------------------*/	
+		CMutex* ELE_mutex = new CMutex ("Mutex_General");
+		
+		/*-------------------------------------------------------------------------------------------------------------*/
+		/* Initializing Datapools */
+		/*-------------------------------------------------------------------------------------------------------------*/	
+		string ELE_DPString = "Datapool_" + to_string(static_cast<long long>(myNumber));
+
+		CDataPool ELE_ElevatorStruct_DataPool(ELE_DPString, sizeof(struct elevatorStatus));
+		elevatorStatus* ELE_ElevatorStruct_Local = (struct elevatorStatus*)(ELE_ElevatorStruct_DataPool.LinkDataPool());
+
+		/*-------------------------------------------------------------------------------------------------------------*/
+		/* Initializing Pipes */
+		/*-------------------------------------------------------------------------------------------------------------*/	
+
+		CPipe ELE_Pipe_EleToDisp = "Pipe_DispatcherToEle_" + to_string(static_cast<long long>(myNumber));
+		CPipe ELE_Pipe_DisptoEle = "Pipe_Ele_" + to_string(static_cast<long long>(myNumber)) + "_ToDispatcher";
+
+		/*-------------------------------------------------------------------------------------------------------------*/
+		/*  */
+		/*-------------------------------------------------------------------------------------------------------------*/	
+		printf("%d rendEle %d \n", rendezvousCount,myNumber);
+		ELE_r1.Wait();
+		ClassThread<Elevator> Thread1 (this, &Elevator::Fxn1, ACTIVE, NULL) ;
+		ClassThread<Elevator> Thread2 (this, &Elevator::Fxn2, ACTIVE, NULL) ;
+		ClassThread<Elevator> Thread3 (this, &Elevator::Fxn1, ACTIVE, NULL) ;
+		Thread1.WaitForThread();
+		Thread2.WaitForThread();
+		Thread3.WaitForThread();
+
+		ELE_r2.Wait();
+		return 0;
+	}
+} ;
 
 /******************
 *** MAIN - I/O ****
 ******************/
 
 int main() {
-
-	/*--------------------------------------------------------------------------------------------------------------*/
-	/* Request User Input for Total Number of Elevators */
-	/*--------------------------------------------------------------------------------------------------------------*/		
+	
 	int numElevator = 0;
 	
 	cout << "Input the number of elevators you want: " << endl;
 	cin >> numElevator;
-	cout << "user entered: " << numElevator << endl;
+	cout << "user entered: " << numElevator << endl;;
 	Elevator ** ele;
 	ele = new Elevator*[numElevator];
-	
+	int rendezvousCount = numElevator + 2;
 
 	/*--------------------------------------------------------------------------------------------------------------*/
 	/* Initializing Rendezvous */
 	/*--------------------------------------------------------------------------------------------------------------*/	
-	int rendezvousCount = numElevator + 2;
-
 	CRendezvous	IO_r1	("Rendezvous_InitiateActiveClasses", rendezvousCount); 
 	CRendezvous IO_r2	("Rendezvous_TerminateClasses", rendezvousCount);
 
 	/*--------------------------------------------------------------------------------------------------------------*/
 	/* Initializing Pipelines */
 	/*--------------------------------------------------------------------------------------------------------------*/
-	CPipe	pipe1	("PipeIOToDispatcher", 1024) ; //defaulting to int.
+	CPipe	pipe1	("PipeIOToDispatcher", 1024) ;
 	CPipe	pipe2	("PipeDispatcherToIO", 1024) ;
 
 	int pipe1data = 0;
@@ -83,8 +261,10 @@ int main() {
 	}
 
 	
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! gotta do smae thing for pipes
+
 	/*--------------------------------------------------------------------------------------------------------------*/
-	/* Instantiating ActiveClasses */
+	/* Instantiating Classes */
 	/*--------------------------------------------------------------------------------------------------------------*/
 	for (int i = 0; i < numElevator; i++) {
 		ele[i] = new Elevator(i,rendezvousCount);
@@ -100,11 +280,6 @@ int main() {
 
 	Sleep(10);
 	
-	/*--------------------------------------------------------------------------------------------------------------*/
-	/* Start Rendezvous with main thread and all ActiveClasses */
-	/*                                                                                                              */
-	/*--------------------------------------------------------------------------------------------------------------*/
-
 	IO_r1.Wait();
 	
 	cout << "let's go!" << endl;
@@ -114,8 +289,7 @@ int main() {
 	/* IO Logic */
 	/* - Goal is to send all keyboard instructions to the dispatcher to decide what to do                           */
 	/*--------------------------------------------------------------------------------------------------------------*/
-
-	bool flag = TRUE;
+	bool flag = false;
 	char KeyData;
 
 	while( flag )
